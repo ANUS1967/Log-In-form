e18bc63432e96bc9385bccbcc4cfc76b45adb694
MUHAMMAD-ANUS <muhammadanus1967@gmail.com>
2025-07-25 13:16:40 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+alert("THANkS FOR VISITING MUHAMMAD ANUS LOG IN FORM");
+document.addEventListener("DOMContentLoaded", function() {
+    const submitBtn = document.querySelector(".btn");
+
+    submitBtn.addEventListener("click", function() {
+        // alert("Form submitted successfully!");
+        window.location.href = "thankyou.html";
+    });
+});
